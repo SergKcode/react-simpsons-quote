cb38b4381e767aa8db74156c2b2d800ac9551c49
unknown <sergio-caballero_student2021@wilder.school>
2021-05-19 12:09:58 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import QuoteCard from './components/QuoteCard';
-import './App.css';
 import React, {useState} from "react";
 import { useAxios } from './hook/useAxios';
+import styled from "styled-components"; 
+
+const StyledContainer = styled.div`
+  display:flex;
+  width:100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items:  center;
+  margin: 10px;
+`;
+const StyledButton = styled.button`
+  background-color: #170AF8;
+  font-size: 2rem;
+  color: white;
+  border-radius: 10px;
+  padding:10px;
+  width:25%;
+  margin-top:10px;
+  &:hover{
+    background-color: #303AF0;
+  }
+`;
+
+
+const StyledH1 = styled.h1`
+  text-shadow: 3px 1px 0px rgba(0, 0, 0, 1);
+  font-weight: bold;
+  font-size: 4em;
+  color: #F7F31D;
+`;
 
 function App() {
   const [change, setChange] = useState(false)
-  const  {data, loaded} = useAxios('https://simpsons-quotes-api.herokuapp.com/quotes' , change)
+  const  {data} = useAxios('https://simpsons-quotes-api.herokuapp.com/quotes' , change)
   const handleButton = ()=>{
     setChange(!change)
 
@@ -13,9 +42,13 @@ function App() {
 
   return (
     <>
-      <h1>Simpson quote</h1>
-      {loaded && <QuoteCard data={data} loaded={loaded}/>}
-      <button onClick={handleButton}></button>
+      <StyledContainer>
+        <StyledH1>Simpson quote</StyledH1>
+      </StyledContainer>
+      <StyledContainer>
+        {data!==null && <QuoteCard data={data} />}
+        <StyledButton onClick={handleButton}> <i>Get a new quote</i></StyledButton>
+      </StyledContainer>
       
     </>
   );
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import './QuoteCard.css';
+import styled from "styled-components"; 
 
-const QuoteCard = ({ data}) => {
+const StyledFigure = styled.figure`
+  display: flex;
+  padding: 1.25em;
+  width: 400px;
+  border: 1px solid #f8f8f8;
+  border-radius: 10px;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.15);
+  font-size: 1.2em;
+  background-color: #FFFFFF;
+`;
+
+const StyledCite = styled.cite`
+  margin-left: 50px;
+`;
+
+const StyledImg= styled.img`
+   max-width: 100px;
+  margin-right: 1.5rem;
+`;
+
+
+
+const QuoteCard = ({data}) => {
 
  const {quote, character, image} = data[0]
 
 
-
-
 return(
   
-  <figure className="QuoteCard">
-    <img src={image} alt={character} />
+  <StyledFigure>
+    <StyledImg src={image} alt={character} />
     <figcaption>
       <blockquote>{quote}</blockquote>
-      <cite>{character}</cite>
+      <StyledCite>{character}</StyledCite>
     </figcaption>  
-  </figure>
+  </StyledFigure>
 );
 }
 
